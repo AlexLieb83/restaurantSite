b28851d1b5bd3f499053ad48555fbc600b9549ce
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-10-05 19:07:38 -0500

    working on tab menu js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // tab menu
 function openMenu(event, menuName) {
   let tabsArray = document.getElementsByClassName("menu");
-  tabsArray.forEach((tab) => {
-    tab.style.display = "none";
-  });
+  for (let i = 0; i < tabsArray.length; i++) {
+    tabsArray[i].style.display = "none";
+  }
 
   let tabLinks = document.getElementsByClassName("tablink");
-  tabLinks.forEach((tab) => {
-    tab.classlist.remove("active-tab");
-  });
+  for (let i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].classList.remove("active-tab");
+  }
+
+  document.getElementById(menuName).style.display = "block";
+  event.currentTarget.classList.add("active-tab");
 }
+
+document.getElementById("mainLink").click();
